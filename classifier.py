44295ad1bc58f23195fcebbd4aa08998e43da083
Hanson Chan <Hanson@Hansons-MacBook-Pro.local>
2022-12-14 04:05:33 +0800

    commit

--- classifier.py
+++ classifier.py
@@ -1,12 +1,11 @@
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
-import keras.optimizers as opt
 import pandas as pd
 import numpy as np
 import handle_inside_data
 import matplotlib.pyplot as plt
-
+#prepare input data
 #prepare input data
 inside_data = pd.read_excel('processed_inside/114833.xlsx')
 outside_data = pd.read_excel('processed_outside/141235.xlsx')
@@ -21,13 +20,13 @@ for i in range(len(inside)):
 for g in range(len(outside)):
     y_outside.append(0)
 
-Y_train = y_inside[:100] + y_outside[:-100]
-Y_test = y_inside[100:] + y_outside[-100:]
+Y_train = y_inside[:1500] + y_outside[:-100]
+Y_test = y_inside[1500:] + y_outside[-100:]
 Y_train = np.array(Y_train)
 Y_test = np.array(Y_test)
 
-X_train = inside[:100] + outside[:-100]
-X_test = inside[100:] + outside[-100:]
+X_train = inside[:1500] + outside[:-100]
+X_test = inside[1500:] + outside[-100:]
 X_train = np.array(X_train)
 X_test = np.array(X_test)
 
@@ -36,18 +35,19 @@ classifier = Sequential()
 classifier.add(Dense(units = 16, activation = 'relu', input_dim = 4))
 classifier.add(Dense(units = 8, activation = 'relu'))
 classifier.add(Dense(units = 4, activation = 'relu'))
+#classifier.add(Dense(units = 2, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics=['accuracy'])
-history = classifier.fit(X_train, Y_train, validation_split=0.33, epochs=150, batch_size=1, verbose=0, shuffle= True)
+history = classifier.fit(X_train, Y_train, batch_size = 1, epochs = 2500, shuffle= True)
 
-loss, accuracy = classifier.evaluate(X_test)
-#Y_pred = classifier.predict(X_test)
+Y_pred = classifier.predict(X_test)
+loss, accuracy = classifier.evaluate(X_test, Y_test)
 
 print(history.history.keys())
 # summarize history for accuracy
 plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
+#plt.plot(history.history['val_accuracy'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
@@ -55,7 +55,7 @@ plt.legend(['train', 'test'], loc='upper left')
 plt.show()
 # summarize history for loss
 plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
+#plt.plot(history.history['val_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
